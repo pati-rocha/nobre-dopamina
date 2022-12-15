6cf6ca6d00b0ddd353bd7ad39aa55e12532300f7
Pati Rocha <patirocha.dev@gmail.com>
2022-12-15 16:09:48 -0300

    routes

---
+++ src/Router.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Menu } from "./components/Menu/menu";
+import { Footer } from "./components/Footer/footer";
+import { Home } from "./Pages/Home/home";
+import { About } from "./Pages/About/about";
+import { Communit } from "./Pages/Communit/communit";
+import { Login } from "./Pages/Login/login";
+import { Error } from "./Pages/Error/error";
+
+import { Private } from "./routes/Private";
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/sobre" element={<About />} />
+
+        <Route path="/comunidade" element={<Private><Communit /></Private>} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
