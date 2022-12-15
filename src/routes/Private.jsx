5826108c072ba5e3f93eb4f7eec5cff36b461565
Pati Rocha <patirocha.dev@gmail.com>
2022-12-15 16:10:42 -0300

    route private

---
+++ src/routes/Private.jsx
@@ -0,0 +1,50 @@
+
+import { useState, useEffect } from "react";
+import { auth } from "../services/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+import { Navigate } from "react-router-dom";
+import { async } from "@firebase/util";
+
+export function Private({children}){
+    const [loading, setLoading] = useState(true);
+    const [signed, setSigned] = useState(false);
+
+
+    useEffect(() => {
+        
+        async function checkLogin(){
+            const unsub = onAuthStateChanged( auth, (user) => {
+                
+                if(user){
+                    const userData = {
+                        uid: user.uid,
+                        email: user.email
+                    }
+                    localStorage.setItem("@detailUser", JSON.stringify(userData))
+                    setLoading(false)
+                    setSigned(true)
+                } else{
+                    setLoading(false)
+                    setLoading(false)
+                }
+            })
+        }
+        checkLogin();
+    },[])
+
+    if(loading){
+        return(
+            <h1>Carregando</h1>
+        )                 
+    }
+
+    if(!signed){
+        return(
+            <Navigate to="/login"/>
+        )
+    }
+   
+    return children;
+
+   
+}
