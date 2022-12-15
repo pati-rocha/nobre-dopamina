9d02fec3e2bfa768572d3ed6929d6932932c6091
Pati Rocha <patirocha.dev@gmail.com>
2022-12-15 16:11:21 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import './global.css'
+
+import { Router } from './Router'
+
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer} from 'react-toastify';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ToastContainer autoClose={1500} />
+    <Router />
   </React.StrictMode>
 )
